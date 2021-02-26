56884d7585070949156d5f1c247fff2e5c125f39
MaxFestersen <49905988+MaxFestersen@users.noreply.github.com>
2021-02-26 14:18:54 +0100

    Altered logic for random day filter. Added documentation. Applied filter. Added section names.

--- Assignment 1.py
+++ Assignment 1.py
@@ -38,25 +38,46 @@ print(crypto_curr) # Print results
 # 2017-06-01 - 2018-01-01 (Close values)
 
 #%% Exercise 2.2
+#%% Exercise 2.2 a
 pd_data = new_data[['date', 'name', 'close']] # Filter to date, name and close
 
 # Choosing a random date
 unique_days = pd_data["date"].unique() # filter unique days in data
+last_day = datetime.strptime(unique_days[-1], "%Y-%m-%d") # Get last date entry in date format (to make date calculations)
+last_available_date = last_day - relativedelta(months=+6) # Get last date that is at least 6 months from end date
+last_available_date = last_available_date.strftime("%Y-%m-%d") # Format as string
+unique_days_filter = unique_days <= last_available_date # create filter list
+unique_days = unique_days[unique_days_filter] # filter unavailable dates (not with 6 months after)
 n_unique_days = len(unique_days) # count unique days
-
 n_randdom_day = random.randint(0,n_unique_days-1) # Select random entry number
 unique_day = unique_days[n_randdom_day] # get entry
+unique_day_until = datetime.strptime(unique_day, "%Y-%m-%d") + relativedelta(months=+6) # Find 6 months after random date
+unique_day_until = unique_day_until.strftime("%Y-%m-%d") # Format as string
 
-is_date = pd_data['date']==unique_day # create filtering list
-pd_data = pd_data[is_date] # Filter data by date
-#r_d_pd_data = pd_data["date" == unique_day] # filter by random day
+# Print random date information:
+print("The random chosen date is: " + unique_day)
 
 
 # Choose a random N of cryptos
-top_currencies = crypto_curr.index.values # top 50 curencies
+top_currencies = crypto_curr.index.values # top 50 curencies - from 2.1
 random_top_curr = set(random.choices(top_currencies, k=random.randint(1,50))) # Select random number of currensies
+weigth_random_top_curr = len(random_top_curr) # Amount of currencsies
+print("There are ", weigth_random_top_curr, " currencies.\nThe random top currencies are:")
+random_top_curr
 
+#%% Exercise 2.2 a filtering
+# Filter the days
+is_dates = pd_data['date'] >= unique_day # create filtering list from random date
+pd_data = pd_data[is_dates] # Filter by the random date and after
+is_dates = pd_data['date'] <= unique_day_until # create filtering list until 6 months after random date
+pd_data = pd_data[is_dates] # Filter by the random date and after
 
+# Filter by top currensies
+curr_in = pd_data['name'].isin(random_top_curr) # Create filter list by random top currensies
+pd_data = pd_data[curr_in] # Apply filter list
+
+#%% Exercise 2.2 b
+n = 1/weigth_random_top_curr
 
 
 
