4038e01b0f20e73d778a915d893a896a0972f075
MaxFestersen <49905988+MaxFestersen@users.noreply.github.com>
2021-02-27 14:13:54 +0100

    Fixed appeding to result dict. ex2c.

--- Assignment 1.py
+++ Assignment 1.py
@@ -32,7 +32,7 @@ results = { # generate dictionary to carry information for each iteration
     "portfolio_volatility" : [],
     "sharpe_ratio" : []
 }
-results["i"] = 0 # First result is created outside of loop
+results["i"].append(0) # First result is created outside of loop
 
 # Set inital data
 pd_data = new_data[['date', 'name', 'close']] # Filter to date, name and close
@@ -100,18 +100,18 @@ for item in random_top_curr:
         print(item + " removed because it was emtpty for holding period. N is now: " + str(len(ret.columns)))
 weigth = np.array(1/len(ret.columns)) # calculate 1/N weights
 
-results["N"] = len(ret.columns)
+results["N"].append(len(ret.columns))
 
 weight_array = np.full((len(ret.columns), 1), weigth)
 
 # Portfolio return, Portfolio volatility, and Sharpe ratio
 # Portfolio volatility (for holding period)
-ret.mean() * 126 # Calculate mean of return (from half of an year)
-ret.cov() * 126 # Calculate covariance matrix for stocks
+#ret.mean() * 126 # Calculate mean of return (from half of an year)
+#ret.cov() * 126 # Calculate covariance matrix for stocks
 
-np.sum(ret.mean().values * weight_array) * 126
-np.dot(weight_array.T, np.dot(ret.cov() * 126, weight_array))
-math.sqrt(np.dot(weight_array.T, np.dot(ret.cov() * 126, weight_array )))
+#np.sum(ret.mean().values * weight_array) * 126
+#np.dot(weight_array.T, np.dot(ret.cov() * 126, weight_array))
+#math.sqrt(np.dot(weight_array.T, np.dot(ret.cov() * 126, weight_array )))
 
 def port_ret(weight, retmean):
     return np.sum(retmean * weigth.T) * 126
@@ -128,20 +128,74 @@ def port_vol(weight, retcov):
     return np.sqrt(sum(vals))
 
 # Add to array (for ex2 c and ex2 d)
-results["portfolio_return"] = port_ret(weight_array, ret.mean())
-results["portfolio_volatility"] = port_vol(weight_array, ret.cov())
+results["portfolio_return"].append(port_ret(weight_array, ret.mean()))
+results["portfolio_volatility"].append(port_vol(weight_array, ret.cov()))
 
 # Sharpe ratio (for holding period)
 Sharpe_Ratio = ret.mean()/ret.std()
 Holding_SR = (126**0.5) * sum(Sharpe_Ratio)
-results["sharpe_ratio"] = Holding_SR
+results["sharpe_ratio"].append(Holding_SR)
 
 #%% Exercise 2.c
 #for i in range(1,10000):
-for i in range(1,5):
-    # Ready to rumble
+for i in range(1,500):
+    # Set inital data
+    pd_data = new_data[['date', 'name', 'close']]  # Filter to date, name and close
 
+    results["i"].append(i)
 
+    # Filtering
+    # Choosing a random date
+    n_randdom_day = random.randint(0, n_unique_days - 1)  # Select random entry number
+    unique_day = unique_days[n_randdom_day]  # get entry
+    unique_day_until = datetime.strptime(unique_day, "%Y-%m-%d") + relativedelta(months=+6)  # Find 6 months after random date
+    unique_day_until = unique_day_until.strftime("%Y-%m-%d")  # Format as string
+
+    # Choose a random N of cryptos
+    random_top_curr = crypto_curr.index.values  # top 50 curencies - from 2.1
+    for n in range(random.randint(0, 49)):  # Randomly run from 0 to 49 times (so at least 1 value is left)
+        random_top_curr = np.delete(random_top_curr, random.randint(0, len(random_top_curr) - 1), 0)  # remove a random value
+
+    weigth_random_top_curr = len(random_top_curr)  # Amount of currencsies
+
+    # Filter the days
+    is_dates = pd_data['date'] >= unique_day  # create filtering list from random date
+    pd_data = pd_data[is_dates]  # Filter by the random date and after
+    is_dates = pd_data['date'] <= unique_day_until  # create filtering list until 6 months after random date
+    pd_data = pd_data[is_dates]  # Filter by the random date and after
+
+    # Filter by top currensies
+    curr_in = pd_data['name'].isin(random_top_curr)  # Create filter list by random top currencies
+    pd_data = pd_data[curr_in]  # Apply filter list
+
+    # Formating and calculations
+    pd_data_1 = pd_data.pivot_table(index='date', columns='name', values='close')  # Set names as columns, close as values with date as index
+    ret = np.log(pd_data_1 / pd_data_1.shift(1))  # Calculate log-return of different stocks
+
+    ret = ret.dropna(axis=1, how='all').dropna(axis=0, how='all')  # Handling missing values, drop column/row if empty
+    if len(ret.columns) == 0: # if there are no columns set vals as NaN
+        results["portfolio_return"].append(float("NaN"))
+        results["portfolio_volatility"].append(float("NaN"))
+        results["sharpe_ratio"].append(float("NaN"))
+    else:
+        weigth = np.array(1 / len(ret.columns))  # calculate 1/N weights
+        results["N"].append(len(ret.columns))
+
+        weight_array = np.full((len(ret.columns), 1), weigth)
+
+        # Portfolio return, Portfolio volatility, and Sharpe ratio
+        # Portfolio return (for holding period)
+        results["portfolio_return"].append(port_ret(weight_array, ret.mean()))
+
+        # Portfolio volatility (for holding period)
+        results["portfolio_volatility"].append(port_vol(weight_array, ret.cov()))
+
+        # Sharpe ratio (for holding period)
+        Sharpe_Ratio = ret.mean() / ret.std()
+        Holding_SR = (126 ** 0.5) * sum(Sharpe_Ratio)
+        results["sharpe_ratio"].append(Holding_SR)
+
+#print(results)
 #%% Excersice 2.d
 #ex2 = plt.plot(results["N"], results["portfolio_volatility"]) # En af jer må lige afprøve. Kan ikke selv få plots til at køre i pycharm. Mvh. Max.
 
