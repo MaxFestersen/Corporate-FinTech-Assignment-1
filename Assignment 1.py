fed03c6cf0885c0c77c2229ac2ef258f25ebe24f
MaxFestersen <49905988+MaxFestersen@users.noreply.github.com>
2021-02-28 20:53:24 +0100

    Wip

--- Assignment 1.py
+++ Assignment 1.py
@@ -9,9 +9,7 @@ import random
 from matplotlib.pylab import mpl, plt
 from datetime import datetime
 from dateutil.relativedelta import relativedelta
-import webbrowser # Skal kun bruges til at vise hjemmeside til opgave 3
 import scipy.optimize as sco
-#import PyPortfolioOpt
 
 #%% Design
 plt.style.use('fivethirtyeight')
@@ -100,7 +98,7 @@ pd_data = pd_data[curr_in] # Apply filter list
 pd_data_1 = pd_data.pivot_table(index = 'date', columns = 'name', values = 'close') # Set names as columns, close as values with date as index
 ret = np.log(pd_data_1/pd_data_1.shift(1)) # Calculate log-return of different stocks
 
-ret = ret.dropna(axis = 1, how = 'all').dropna(axis = 0, how = 'all') # Handling missing values, drop column/row if empty
+ret = ret.dropna(axis=1, how='all').dropna(axis=0, how='any')  # Handling missing values, drop any column with a nan
 for item in random_top_curr:
     if(item not in ret.columns):
         print(item + " removed because it was empty for holding period.")
@@ -123,8 +121,8 @@ weight_array = np.full((len(ret.columns), 1), weight) # Create array with weight
 #np.dot(weight_array.T, np.dot(ret.cov() * 126, weight_array))
 #math.sqrt(np.dot(weight_array.T, np.dot(ret.cov() * 126, weight_array )))
 
-def port_ret(weight, retmean):
-    return np.sum(retmean * weight) * 126
+def port_ret(weight_array):
+    return np.sum(np.array(ret.mean().values) * weight_array) * 126
 
 def port_vol(weight):
     p_var = np.dot(
@@ -136,7 +134,7 @@ def port_vol(weight):
     return(p_var)
 
 # Add to array (for ex2 c and ex2 d)
-results["portfolio_return"].append(port_ret(weight, ret.mean()))
+results["portfolio_return"].append(port_ret(weight_array))
 results["portfolio_volatility"].append(np.sum(port_vol(weight_array)))
 
 # Sharpe ratio (for holding period)
@@ -145,12 +143,11 @@ Holding_SR = (126**0.5) * sum(Sharpe_Ratio)
 results["sharpe_ratio"].append(Holding_SR)
 
 #%% Exercise 2.2.c
-#for i in range(1,10000):
-for i in range(1,5):
+for i in range(1,10000):
+#for i in range(1,5):
+    results["i"].append(i)
     # Set inital data
     pd_data = new_data[['date', 'name', 'close']]  # Filter to date, name and close
-
-    results["i"].append(i)
 
     # Filtering
     # Choosing a random date
@@ -180,7 +177,12 @@ for i in range(1,5):
     pd_data_1 = pd_data.pivot_table(index='date', columns='name', values='close')  # Set names as columns, close as values with date as index
     ret = np.log(pd_data_1 / pd_data_1.shift(1))  # Calculate log-return of different stocks
 
-    ret = ret.dropna(axis=1, how='all').dropna(axis=0, how='all')  # Handling missing values, drop column/row if empty
+    ret = ret.dropna(axis=1, how='all').dropna(axis=0, how='all')  # Handling missing values, drop any column with a nan
+    for name, values in ret.cov().iteritems():
+        curr = values.values
+        curr[np.isnan(values)] = 0
+        if sum(curr) == 0:
+            ret = ret.drop(columns=[name])
     if len(ret.columns) == 0: # if there are no columns set vals as NaN
         results["N"].append(0)
         results["portfolio_return"].append(0)
@@ -194,7 +196,7 @@ for i in range(1,5):
 
         # Portfolio return, Portfolio volatility, and Sharpe ratio
         # Portfolio return (for holding period)
-        results["portfolio_return"].append(port_ret(weight, ret.mean()))
+        results["portfolio_return"].append(port_ret(weight_array))
 
         # Portfolio volatility (for holding period)
         results["portfolio_volatility"].append(np.sum(port_vol(weight_array)))
@@ -210,8 +212,6 @@ ex2 = plt.scatter(results["N"], results["portfolio_volatility"])
 plt.show()
 
 #%% Excersice 2.3
-#webbrowser.open('https://randerson112358.medium.com/python-for-finance-portfolio-optimization-66882498847')  # Go to example.com
-
 #%% Excersice 2.3.a
 results_2_3 = { # generate dictionary to carry information for each iteration
     "i" : [], # Iteration
@@ -259,7 +259,7 @@ pd_data = pd_data[curr_in] # Apply filter list
 pd_data_1 = pd_data.pivot_table(index = 'date', columns = 'name', values = 'close') # Set names as columns, close as values with date as index
 ret = pd_data_1/pd_data_1.shift(1) # Calculate log-return of different stocks
 
-ret = ret.dropna(axis = 1, how = 'all').dropna(axis = 0, how = 'all') # Handling missing values, drop column/row if empty
+ret = ret.dropna(axis=1, how='all').dropna(axis=0, how='all')  # Handling missing values, drop any column with a nan
 for item in random_top_curr:
     if(item not in ret.columns):
         print(item + " removed because it was empty for holding period.")
@@ -267,91 +267,101 @@ for item in random_top_curr:
 if (len(ret.columns) < weight_random_top_curr):
     print("N is now: " + str(len(ret.columns)))
 
-#%% Calculations for exercise
-results["N"].append(len(ret.columns)) # Append Number of columns to results dictionary
+#%% Excersice 2.3.b
+results_2_3["N"].append(len(ret.columns)) # Append Number of columns to results dictionary
 
 # Sharpe ratio (for holding period)
 Sharpe_Ratio = ret.mean()/ret.std() # Calculate sharpe ratio for each currency
 Holding_SR = (126**0.5) * Sharpe_Ratio # Account for holding period
-results["sharpe_ratio"].append(sum(Holding_SR))
 
 weight = np.array(1/len(ret.columns)) # Calculate 1/N weights
-weight_array = np.full((len(ret.columns), 1), weight) # Create array with weight N times
+weight_array = np.full((len(ret.columns), 1), weight)  # Create array with weight N times
 
-def min_func_sharpe(weight, retmean, weight_array):
-    min_sharpe = -port_ret(weight, retmean) / np.sum(port_vol(weight_array))
+
+def min_func_sharpe(weight_array):
+    print("her")
+    print(weight_array)
+    min_sharpe = -port_ret(weight_array) / np.sum(port_vol(weight_array))
     return(min_sharpe)
 
-def min_func_sharpe(weight):
-    min_sharpe = -port_ret(weight, ret.mean()) / np.sum(port_vol(weight_array))
-    return(min_sharpe)
-
-#test = min_func_sharpe(weight, ret.mean(), weight_array, ret.cov())
 cons = ({'type': 'eq', 'fun': lambda x:  np.sum(x) - 1})
 bnds = tuple((0, 1) for x in range(len(ret.columns)))
 
-#opts = sco.minimize(min_func_sharpe, weight, ret.mean(), weight_array, ret.cov(), method='SLSQP', bounds=bnds, constraints=cons)
 opts = sco.minimize(min_func_sharpe, weight, method='SLSQP', bounds=bnds, constraints=cons)
-
-optv = sco.minimize(port_vol, weight_array,
-                    method='SLSQP', bounds=bnds,
-                    constraints=cons)
-
-#returns = ret.pct_change()  # SIMPLE RETURN AND NOT LOG RETURN
-#cov_matrix_semi_annual = returns.cov() * 126 # Account for holding period
-
-
-
-
-
-
-
-
-
-
-
-# Forsøg 1
-#port_variance = np.dot(weights.T, np.dot(cov_matrix_semi_annual, weights))
-#port_volatility = np.sqrt(port_variance)
-#portfolioSimpleSemiAnnualReturn = np.sum(returns.mean()*weights) * 126
-
-#percent_var = str(np.round(port_variance, 2) * 100) + '%'
-#percent_vols = str(np.round(port_volatility, 2) * 100) + '%'
-#percent_ret = str(np.round(portfolioSimpleSemiAnnualReturn, 2)*100)+'%'
-#print("Expected annual return : "+ percent_ret) # Det her er ikke rigtigt
-#print('Annual volatility/standard deviation/risk : '+percent_vols) # Det her virker heller ikke rigtigt
-#print('Annual variance : '+percent_var) # Det her virker heller ikke rigtigt.
-
-#from pypfopt.efficient_frontier import EfficientFrontier
-#from pypfopt import risk_models
-#from pypfopt import expected_returns
-
-#mu = expected_returns.mean_historical_return(ret) #returns.mean() * 252
-#S = risk_models.sample_cov(ret) #Get the sample covariance matrix
-
-#ef = EfficientFrontier(mu, S)
-
-#weights = ef.max_sharpe() #Maximize the Sharpe ratio, and get the raw weights
-#cleaned_weights = ef.clean_weights()
-
-#print(cleaned_weights) #Note the weights may have some rounding error, meaning they may not add up exactly to 1 but should be close
-#ef.portfolio_performance(verbose=True)
-# Forsøg 1 slut
+weight_array = opts["x"].T
 
 # Portfolio return, Portfolio volatility, and Sharpe ratio
-
-# Add to array (for ex2 c and ex2 d)
-#results["portfolio_return"].append(port_ret(weight_array, ret.mean()))
-#results["portfolio_volatility"].append(port_vol(weight_array))
-
-#%% Excersice 2.3.b
-
+# Add to array (for ex2.3 c and ex2.3 d)
+results_2_3["portfolio_return"].append(port_ret(weight_array))
+results_2_3["portfolio_volatility"].append(np.sum(port_vol(weight_array)))
+results_2_3["sharpe_ratio"].append(port_ret(weight_array)/np.sum(port_vol(weight_array)))
 
 #%% Excersice 2.3.c
+#for i in range(1,10000):
+for i in range(1,5):
+    # Set inital data
+    pd_data = new_data[['date', 'name', 'close']]  # Filter to date, name and close
 
+    # Choosing a random date
+    n_randdom_day = random.randint(0, n_unique_days - 1)  # Select random entry number
+    unique_day = unique_days[n_randdom_day]  # get entry
+    unique_day_until = datetime.strptime(unique_day, "%Y-%m-%d") + relativedelta(months=+6)  # Find 6 months after random date
+    unique_day_until = unique_day_until.strftime("%Y-%m-%d")  # Format as string
+
+    # Choose a random N of cryptos
+    random_top_curr = crypto_curr.index.values  # top 50 curencies - from 2.1
+    for n in range(random.randint(0, 49)):  # Randomly run from 0 to 49 times (so at least 1 value is left)
+        random_top_curr = np.delete(random_top_curr, random.randint(0, len(random_top_curr) - 1), 0)  # remove a random value
+
+    weight_random_top_curr = len(random_top_curr)  # Amount of currencsies
+
+    # Filter the days
+    is_dates = pd_data['date'] >= unique_day  # create filtering list from random date
+    pd_data = pd_data[is_dates]  # Filter by the random date and after
+    is_dates = pd_data['date'] <= unique_day_until  # create filtering list until 6 months after random date
+    pd_data = pd_data[is_dates]  # Filter by the random date and after
+
+    # Filter by top currencies
+    curr_in = pd_data['name'].isin(random_top_curr)  # Create filter list by random top currencies
+    pd_data = pd_data[curr_in]  # Apply filter list
+    pd_data_1 = pd_data.pivot_table(index='date', columns='name', values='close')  # Set names as columns, close as values with date as index
+    ret = pd_data_1 / pd_data_1.shift(1)  # Calculate log-return of different stocks
+    ret = ret.dropna(axis=1, how='all').dropna(axis=0, how='all')  # Handling missing values, drop any column with a nan
+
+    for name, values in ret.cov().iteritems(): # Drop values that have no cov
+        curr = values.values # Get values
+        curr[np.isnan(values)] = 0 # Set to 0
+        if sum(curr) == 0:
+            ret = ret.drop(columns=[name])
+
+    if len(ret.columns) == 0: # if there are no columns set vals as NaN
+        results_2_3["i"].append(i)  # First result is created outside of loop
+        results_2_3["N"].append(0)
+        results_2_3["portfolio_return"].append(0)
+        results_2_3["portfolio_volatility"].append(0)
+        results_2_3["sharpe_ratio"].append(0)
+    else:
+        results_2_3["i"].append(i)  # First result is created outside of loop
+        results_2_3["N"].append(len(ret.columns))  # Append Number of columns to results dictionary
+
+        # Sharpe ratio (for holding period)
+        Sharpe_Ratio = ret.mean() / ret.std()  # Calculate sharpe ratio for each currency
+        #Holding_SR = (126 ** 0.5) * Sharpe_Ratio  # Account for holding period
+
+        weight = np.array(1 / len(ret.columns))  # Calculate 1/N weights
+        weight_array = np.full((len(ret.columns), 1), weight)  # Create array with weight N times
+
+        opts = sco.minimize(min_func_sharpe, weight_array, method='SLSQP', bounds=bnds, constraints=cons)
+        weight_array = opts["x"].T
+
+        # Portfolio return, Portfolio volatility, and Sharpe ratio
+        # Add to array (for ex2.3 c and ex2.3 d)
+        results_2_3["portfolio_return"].append(port_ret(weight_array))
+        results_2_3["portfolio_volatility"].append(np.sum(port_vol(weight_array)))
+        results_2_3["sharpe_ratio"].append(port_ret(weight_array) / np.sum(port_vol(weight_array)))
 
 #%% Excersice 2.3.d
-#ex3 = plt.plot(results["N"], results_ex3["portfolio_volatility"]) # En af jer må lige afprøve. Kan ikke selv få plots til at køre i pycharm. Mvh. Max.
+#ex3 = plt.plot(results_2_3["N"], results_2_3["portfolio_volatility"]) # En af jer må lige afprøve. Kan ikke selv få plots til at køre i pycharm. Mvh. Max.
 
 
 #%% Excersice 2.4
